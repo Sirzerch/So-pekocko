7076fa52cb80192f2ed4f4261823f2dbe8e46f53
Sirzerch <eniquentind@gmail.com>
2021-01-13 11:18:43 +0100

    Mise en place de la fonctionnalité like/dislike

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -30,7 +30,7 @@ exports.getOneSauce = (req, res, next) => {
 
 exports.modifySauce = (req, res, next) => {
     const sauceObject = req.file ?
-        { 
+        {
             ...JSON.parse(req.body.sauce),
             imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
         } : { ...req.body };
@@ -50,4 +50,57 @@ exports.deleteSauce = (req, res, next) => {
             });
         })
         .catch(error => res.status(500).json({ error }));
+};
+
+exports.likeSauce = (req, res, next) => {
+    const uid = req.body.userId;
+    const like = req.body.like;
+
+    Sauce.findOne({ _id: req.params.id })
+        .exec((error, sauce) => {
+            let msg = "";
+            let uiL = sauce.usersLiked.indexOf(uid);
+            let uiD = sauce.usersDisliked.indexOf(uid);
+
+            if (like == 0 && uiL > -1) {
+
+                sauce.likes--;
+                sauce.usersLiked.splice(uiL, 1);
+                msg = "Unliked !";
+
+            } else if (like == 0 && uiD > -1) {
+
+                sauce.dislikes--;
+                sauce.usersDisliked.splice(uiD, 1);
+                msg = "Undisliked !";
+
+            };
+
+            if (like == 1) {
+
+                sauce.likes++;
+                if (sauce.usersLiked.length == 0) {
+                    sauce.usersLiked = [uid];
+
+                } else {
+                    sauce.usersLiked.push(uid);
+                }
+                msg = "Like pris en compte !";
+            };
+
+            if (like == -1) {
+
+                sauce.dislikes++;
+                if (sauce.usersDisliked.length == 0) {
+                    sauce.usersDisliked = [uid];
+                } else {
+                    sauce.usersDisliked.push(uid);
+                }
+                msg = "DisLike pris en compte !";
+
+            };
+            sauce.save()
+                .then(() => res.status(201).json({ message: msg }))
+                .catch(error => res.status(400).json({ error }));
+        });
 };
